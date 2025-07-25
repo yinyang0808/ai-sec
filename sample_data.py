f260b36162555e452a7c2f35b3d4ca102fee1f19
yinyang0808 <haeunc065@gmail.com>
2025-07-25 14:05:21 +0900

    Create sample_data.py

---
+++ sample_data.py
@@ -0,0 +1,26 @@
+sample_data = [
+    {
+        "name": "Quiz",
+        "weight": 30,
+        "scores": [75, 80, 70, 90, 60],
+        "total_count": 20
+    },
+    {
+        "name": "Exam",
+        "weight": 40,
+        "scores": [70],
+        "total_count": 3
+    },
+    {
+        "name": "Attendance",
+        "weight": 20,
+        "scores": [100]*41,
+        "total_count": 40
+    },
+    {
+        "name": "Extra Credit",
+        "weight": 3,
+        "scores": [],
+        "total_count": 1
+    }
+]
